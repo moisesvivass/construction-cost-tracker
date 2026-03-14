53d4d35826039c81dbee10ff945b97e324898b26
Moises Vivas <moisesvivass@gmail.com>
2026-03-14 17:16:07 -0400

    security: add CSRF protection, real SECRET_KEY, and update requirements

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +14,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
     login_manager.login_view = "main.login"
     login_manager.login_message = "Please log in to access this page."
